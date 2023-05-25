587ebfd038394c1318e66522b49f3489deab24ad
marknesh <markmunene.nesh@gmail.com>
2023-05-25 17:00:38 +0300

    test:console log magic link url

--- app/utils/firebase-utils.ts
+++ app/utils/firebase-utils.ts
@@ -10,12 +10,13 @@ const actionCodeSettings: ActionCodeSettings = {
   // URL must be in the authorized domains list in the Firebase Console.
   //Please remember to replace the URL with the appropriate value for production.
   url: (process.env.NODE_ENV !== 'production'
-    ? `${process.env.NEXT_PUBLIC_EMAIL_LINK_DEVELOPMENT_URL}`
-    : `${process.env.NEXT_PUBLIC_EMAIL_LINK_PRODUCTION_URL}`) as string,
+    ? process.env.NEXT_PUBLIC_EMAIL_LINK_DEVELOPMENT_URL
+    : process.env.NEXT_PUBLIC_EMAIL_LINK_PRODUCTION_URL) as string,
   handleCodeInApp: true,
 };
 
 export async function sendEmailLink(email: string) {
+  console.log(process.env.NEXT_PUBLIC_EMAIL_LINK_DEVELOPMENT_URL, 'url');
   return new Promise((resolve, reject) => {
     sendSignInLinkToEmail(auth, email, actionCodeSettings)
       .then(() => {
