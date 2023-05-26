2bd9ac39d8cb13269d808a18a84cd032ca45386f
marknesh <markmunene.nesh@gmail.com>
2023-05-26 11:08:38 +0300

    feat: create homepage

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,28 @@
+import Button from '@/components/Button';
 import { checkIfLoggedIn } from '@/utils/checkIfLoggedIn';
+import Link from 'next/link';
 
 export default async function Home() {
   const user = await checkIfLoggedIn();
   return (
-    <div>
-      {user ? <h2>Hi {user?.email}</h2> : <h2>You are not logged in</h2>}
+    <div className='flex flex-col space-y-10 justify-center items-center min-h-[80vh]'>
+      <div className='flex flex-col space-y-3 md:space-y-2 justify-center items-center '>
+        <h2 className='text-3xl font-semibold text-center text-gray-900'>
+          Next.js 13 Firebase Auth Starter Template{' '}
+        </h2>
+        <p className='text-gray-500 text-center'>
+          This is a Next.js starter kit that use Firebase Email Link for simple
+          and secure passwordless login.
+        </p>
+      </div>
+
+      {!user ? (
+        <Link href='/signin'>
+          <Button className='w-32 mt-10'>Login</Button>
+        </Link>
+      ) : (
+        <p className='text-gray-900 text-lg'>You are logged in</p>
+      )}
     </div>
   );
 }
